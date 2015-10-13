45717005a4221e5b8a8439ae456032d4202de867
BarishYumerov <barish.yumerov@gmail.com>
2015-10-13 13:24:45 +0300

    Parser skeleton added

--- ABS/src/Parser.java
+++ ABS/src/Parser.java
@@ -2,7 +2,27 @@ import java.util.ArrayList;
 
 public class Parser {
 	
-	//public Parser(ArrayList<Position> codeOrder, Hash ){
+	private ArrayList<Position> codeOrder;
+	private Hash symbols;
+	private int currentIndex = -1;
+	private Symbol currentSymbol;
+	private Position currentPosition;
+	
+	public Parser(ArrayList<Position> codeOrder, Hash symbols ){
+		this.codeOrder = codeOrder;
+		this.symbols = symbols;
+	}
+	
+	public void codeBlock(){
 		
-	//}
+	}
+	
+	public void operator(){
+		
+	}
+	
+	public void getNextSymbol(){
+		this.currentPosition = codeOrder.get(currentIndex++);
+		// TODO : create get method this.currentSymbol = symbols.get(this.currentPosition., chain)
+	}
 }
