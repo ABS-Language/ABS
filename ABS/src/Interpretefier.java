6e62c902e97d34696cae07218ebf5df273e10d47
hero101 <svetoslavp_23@abv.bg>
2015-12-05 23:24:34 +0200

    interpretator EQU fully(?) implemented.

--- ABS/src/Interpretefier.java
+++ ABS/src/Interpretefier.java
@@ -84,57 +84,161 @@ public class Interpretefier {
 				}
 				
 				case Consts.OPERATORS.EQU : {
-					result = hash.get(currentRow.getResult());
+result = hash.get(currentRow.getResult());
+					
 					op1 = hash.get((Position)currentRow.getOp1());
 					op2 = hash.get((Position)currentRow.getOp2());
 					
 					int temp = 0;
 					
-					switch(result.getType()) {
+					switch(op1.getType()) {
 						case Consts.TYPES.INTEGER : {
-							if(op1.getIntValue() == op2.getIntValue()){
-								temp = 1;
+							int type = op2.getType();
+							
+							if(type == Consts.TYPES.INTEGER) {
+								if(op1.getIntValue() == op2.getIntValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.DOUBLE) {
+								if(op1.getDoubleValue() == op2.getDoubleValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.CHAR) {
+								if(op1.getIntValue() == op2.getCharValue()){
+									temp = 1;
+								}						
+							}
+							else if(type == Consts.TYPES.STRING) {
+								String str = op2.getStringValue();
+								
+								int ascii = 0;
+							
+								for(int i = 0; i < str.length(); i++) {
+									ascii += str.charAt(i);
+								}
+								
+								if(op1.getIntValue() == ascii){
+									temp = 1;
+								}
 							}
 							
-							result.setIntValue(temp);
-							System.out.println(temp);
-							
 							break;
-						}
+						} //case integer
 						
 						case Consts.TYPES.DOUBLE : {
-							if(op1.getDoubleValue() == op2.getDoubleValue()){
-								temp = 1;
+							int type = op2.getType();
+							
+							if(type == Consts.TYPES.INTEGER
+								||	type == Consts.TYPES.DOUBLE) {
+								if(op1.getDoubleValue() == op2.getDoubleValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.CHAR) {
+								if(op1.getDoubleValue() == op2.getCharValue()){
+									temp = 1;
+								}						
+							}
+							else if(type == Consts.TYPES.STRING) {
+								String str = op2.getStringValue();
+								
+								int ascii = 0;
+							
+								for(int i = 0; i < str.length(); i++) {
+									ascii += str.charAt(i);
+								}
+								
+								if(op1.getDoubleValue() == ascii){
+									temp = 1;
+								}
 							}
 							
-							result.setIntValue(temp);
-							System.out.println(temp);
-							
 							break;
-						}
-						
-						case Consts.TYPES.STRING : {
-							if(op1.getStringValue().equals(op2.getStringValue())){
-								temp = 1;
-							}
-							
-							result.setIntValue(temp);
-							System.out.println(temp);
-							
-							break;
-						}
+						} //case double
 						
 						case Consts.TYPES.CHAR : {
-							if(op1.getCharValue() == op2.getCharValue()){
-								temp = 1;
+							int type = op2.getType();
+							
+							if(type == Consts.TYPES.INTEGER) {
+								if(op1.getCharValue() == op2.getIntValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.DOUBLE) {
+								if(op1.getCharValue() == op2.getDoubleValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.CHAR) {
+								if(op1.getCharValue() == op2.getCharValue()){
+									temp = 1;
+								}						
+							}
+							else if(type == Consts.TYPES.STRING) {
+								String str = op2.getStringValue();
+								
+								int ascii = 0;
+							
+								for(int i = 0; i < str.length(); i++) {
+									ascii += str.charAt(i);
+								}
+								
+								if(op1.getCharValue() == ascii){
+									temp = 1;
+								}
 							}
 							
-							result.setIntValue(temp);
-							System.out.println(temp);
+							break;
+						} //case char
+						
+						case Consts.TYPES.STRING : {
+							int type = op2.getType();
+							
+							if(type == Consts.TYPES.INTEGER) {
+								if(op1.getIntValue() == op2.getIntValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.DOUBLE) {
+								if(op1.getDoubleValue() == op2.getDoubleValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.CHAR) {
+								if(op1.getIntValue() == op2.getCharValue()){
+									temp = 1;
+								}						
+							}
+							else if(type == Consts.TYPES.STRING) {
+								String str1 = op1.getStringValue();
+								
+								int ascii1 = 0;
+							
+								for(int i = 0; i < str1.length(); i++) {
+									ascii1 += str1.charAt(i);
+								}
+								
+								String str2 = op2.getStringValue();
+								
+								int ascii2 = 0;
+							
+								for(int i = 0; i < str2.length(); i++) {
+									ascii2 += str2.charAt(i);
+								}
+								
+								if(ascii1 == ascii2){
+									temp = 1;
+								}
+							}
 							
 							break;
-						}
-					}
+						} //case string
+					} //switch op1.getType()
+
+					result.setIntValue(temp);
+					System.out.println(temp);
 					
 					break;
 				}
