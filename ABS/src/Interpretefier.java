2b3f99720227d196855fdb8a2d724e72f474eb4c
hero101 <svetoslavp_23@abv.bg>
2015-12-05 23:20:21 +0200

    interpretator GREATER fully(?) implemented.

--- ABS/src/Interpretefier.java
+++ ABS/src/Interpretefier.java
@@ -139,42 +139,162 @@ public class Interpretefier {
 					break;
 				}
 				
-				case Consts.OPERATORS.GREATER : { //TODO : think of string
+				case Consts.OPERATORS.GREATER : {
 					result = hash.get(currentRow.getResult());
 					
-					switch(result.getType()) {
+					op1 = hash.get((Position)currentRow.getOp1());
+					op2 = hash.get((Position)currentRow.getOp2());
+					
+					int temp = 0;
+					
+					switch(op1.getType()) {
 						case Consts.TYPES.INTEGER : {
-							op1 = hash.get((Position)currentRow.getOp1());
-							op2 = hash.get((Position)currentRow.getOp2());
+							int type = op2.getType();
 							
-							int temp = 0;
+							if(type == Consts.TYPES.INTEGER) {
+								if(op1.getIntValue() > op2.getIntValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.DOUBLE) {
+								if(op1.getDoubleValue() > op2.getDoubleValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.CHAR) {
+								if(op1.getIntValue() > op2.getCharValue()){
+									temp = 1;
+								}						
+							}
+							else if(type == Consts.TYPES.STRING) {
+								String str = op2.getStringValue();
+								
+								int ascii = 0;
 							
-							if(op1.getIntValue() > op2.getIntValue()){
-								temp = 1;
+								for(int i = 0; i < str.length(); i++) {
+									ascii += str.charAt(i);
+								}
+								
+								if(op1.getIntValue() > ascii){
+									temp = 1;
+								}
 							}
 							
-							result.setIntValue(temp);
-							System.out.println(temp);
-							
 							break;
-						}
+						} //case integer
 						
 						case Consts.TYPES.DOUBLE : {
-							op1 = hash.get((Position)currentRow.getOp1());
-							op2 = hash.get((Position)currentRow.getOp2());
+							int type = op2.getType();
 							
-							int temp = 0;
+							if(type == Consts.TYPES.INTEGER
+								||	type == Consts.TYPES.DOUBLE) {
+								if(op1.getDoubleValue() > op2.getDoubleValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.CHAR) {
+								if(op1.getDoubleValue() > op2.getCharValue()){
+									temp = 1;
+								}						
+							}
+							else if(type == Consts.TYPES.STRING) {
+								String str = op2.getStringValue();
+								
+								int ascii = 0;
 							
-							if(op1.getDoubleValue() > op2.getDoubleValue()){
-								temp = 1;
+								for(int i = 0; i < str.length(); i++) {
+									ascii += str.charAt(i);
+								}
+								
+								if(op1.getDoubleValue() > ascii){
+									temp = 1;
+								}
 							}
 							
-							result.setIntValue(temp);
-							System.out.println(temp);
+							break;
+						} //case double
+						
+						case Consts.TYPES.CHAR : {
+							int type = op2.getType();
+							
+							if(type == Consts.TYPES.INTEGER) {
+								if(op1.getCharValue() > op2.getIntValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.DOUBLE) {
+								if(op1.getCharValue() > op2.getDoubleValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.CHAR) {
+								if(op1.getCharValue() > op2.getCharValue()){
+									temp = 1;
+								}						
+							}
+							else if(type == Consts.TYPES.STRING) {
+								String str = op2.getStringValue();
+								
+								int ascii = 0;
+							
+								for(int i = 0; i < str.length(); i++) {
+									ascii += str.charAt(i);
+								}
+								
+								if(op1.getCharValue() > ascii){
+									temp = 1;
+								}
+							}
 							
 							break;
-						}
-					}
+						} //case char
+						
+						case Consts.TYPES.STRING : {
+							int type = op2.getType();
+							
+							if(type == Consts.TYPES.INTEGER) {
+								if(op1.getIntValue() > op2.getIntValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.DOUBLE) {
+								if(op1.getDoubleValue() > op2.getDoubleValue()){
+									temp = 1;
+								}
+							}
+							else if(type == Consts.TYPES.CHAR) {
+								if(op1.getIntValue() > op2.getCharValue()){
+									temp = 1;
+								}						
+							}
+							else if(type == Consts.TYPES.STRING) {
+								String str1 = op1.getStringValue();
+								
+								int ascii1 = 0;
+							
+								for(int i = 0; i < str1.length(); i++) {
+									ascii1 += str1.charAt(i);
+								}
+								
+								String str2 = op2.getStringValue();
+								
+								int ascii2 = 0;
+							
+								for(int i = 0; i < str2.length(); i++) {
+									ascii2 += str2.charAt(i);
+								}
+								
+								if(ascii1 > ascii2){
+									temp = 1;
+								}
+							}
+							
+							break;
+						} //case string
+					} //switch op1.getType()
+
+					result.setIntValue(temp);
+					System.out.println(temp);
 					
 					break;
 				}
@@ -195,7 +315,7 @@ public class Interpretefier {
 					break;
 				}
 				
-				case Consts.OPERATORS.LESS : {//TODO : think of string
+				case Consts.OPERATORS.LESS : {
 					result = hash.get(currentRow.getResult());
 					
 					op1 = hash.get((Position)currentRow.getOp1());
