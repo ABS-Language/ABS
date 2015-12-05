796c89cf605c90b2fc6095841affe1b971baf63b
BarishYumerov <barish.yumerov@gmail.com>
2015-12-06 00:40:07 +0200

    removed unnecessary imports

--- ABS/src/IDE.java
+++ ABS/src/IDE.java
@@ -4,29 +4,20 @@ import java.awt.GridLayout;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.KeyStroke;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.channels.NetworkChannel;
 
 public class IDE extends JFrame{
 	private final int FRAME_WIDTH = 1200;
